169a31cb69661f24689c654d473969f4fd1783c7
Eyvos <56622967+Eyvos@users.noreply.github.com>
2021-03-31 09:06:12 +0200

    fbdfb

--- Programmes + Images/Tuto_pichou/Pichou-tuto-évenement.py
+++ Programmes + Images/Tuto_pichou/Pichou-tuto-évenement.py
@@ -1,11 +1,11 @@
 #coding:utf-8
 import pygame
 
-pygame.init()
-window_resolution = (640,480)
+window_resolution = (640, 480)
 
+pygame.init()
 pygame.display.set_caption("python #40")
-window_surface = pygame.display.set_mode(window_resolution)
+window_surface = pygame.display.set_mode(window_resolution, pygame.RESIZABLE)
 pygame.display.flip()
 
 launched = True
@@ -13,3 +13,8 @@ while launched:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             launched = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                print("Haut")
+            elif event.key == pygame.K_DOWN:
+                print("Bas")
